dd86417dc571166c203d4c2d1e18b5962d0a4c3f
rodrigues1x <lorenzomeni77@gmail.com>
2024-09-05 16:25:00 -0300

    :sparkles: feat:adicionando padrao de cores

--- src/app/components/loading/index.tsx
+++ src/app/components/loading/index.tsx
@@ -1,9 +1,11 @@
 import { ActivityIndicator } from "react-native"
 
 import { styles } from "./styles"
+import { theme } from "@/themes"
 
 export function Loading(){
     return (
-        <ActivityIndicator style={styles.loading}/>
+        <ActivityIndicator style={styles.loading}
+        color={theme.colors.blue}/>
     )
 }
