91ebe1ded6994edd18cfd221af554fb5c44a531d
rodrigues1x <lorenzomeni77@gmail.com>
2024-09-05 15:56:07 -0300

    :sparkles: feat:pagina de login

--- App.tsx
+++ App.tsx
@@ -1,6 +1,24 @@
-import { Home } from '@/app/home';
+import {
+  useFonts,
+  Ubuntu_700Bold,
+  Ubuntu_500Medium,
+  Ubuntu_400Regular
+} from '@expo-google-fonts/ubuntu'
+
+import { Home } from '@/app/components/home';
+import { Loading } from '@/app/components/loading';
 
 export default function App() {
+  const[fontsLoaded] = useFonts({
+    Ubuntu_700Bold,
+    Ubuntu_500Medium,
+    Ubuntu_400Regular
+  })
+
+  if(!fontsLoaded){
+    return <Loading/>
+  }
+
   return <Home />
 }
 
